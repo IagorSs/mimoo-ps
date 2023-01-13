ecb9d6b14d679409b26c1ab2b5d8fac89b4c6618
IagorSs <iagor.penaranda@uol.com.br>
2023-01-12 21:03:41 -0300

    refactor: change test names

--- src/routes/healthcheck/healthcheck.test.ts
+++ src/routes/healthcheck/healthcheck.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-test('healthcheck returns 204', async () => {
+it('should healthcheck returns 204', async () => {
   if (process.env.API_URL === undefined) return fail()
 
   const { status } = await axios.get(`${process.env.API_URL}/healthcheck`)
