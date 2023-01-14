1a940e1787c8cb3198b40c9a863f43dfa3bf6da3
IagorSs <iagor.penaranda@uol.com.br>
2023-01-14 12:52:14 -0300

    refactor(tool-mappers): better exports

---
+++ src/Tool/mappers/index.ts
@@ -0,0 +1,3 @@
+export { default as fromDbToJsClass } from './toJsClass/fromDb'
+export { default as fromGeneralObjToJsClass } from './toJsClass/fromGeneralObj'
+export { default as fromJsClassToGeneralObj } from './toGeneralObj/fromJsClass'
