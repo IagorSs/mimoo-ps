98c948622f883c67f505fc952dd62782619d8e54
IagorSs <iagor.penaranda@uol.com.br>
2023-01-15 19:29:07 -0300

    feat: put errorHandler to lead with AuthAwsErrors

--- src/middlewares/error-handler/index.ts
+++ src/middlewares/error-handler/index.ts
@@ -1,25 +1,54 @@
-import { Response, Body } from '@core/http'
+import { Response, Body, ResponseProps } from '@core/http'
 import middy from '@middy/core'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { ApiError } from '@core/Error'
 
-type PossibleErrors = ApiError | Error
+interface AwsError extends Error {
+  code: string
+}
+
+type PossibleErrors = ApiError | Error | AwsError
 
 export default (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
+  const treatAwsError = (awsError: AwsError): Response => {
+    const responseProps: ResponseProps = {
+      statusCode: 500,
+      body: new Body(`Error creating user - ${awsError.message}`)
+    }
+
+    switch (awsError.code) {
+      case 'UsernameExistsException':
+        responseProps.statusCode = 409
+        break
+      case 'InvalidParameterException':
+      case 'InvalidPasswordException':
+        responseProps.statusCode = 400
+        break
+    }
+
+    return new Response(responseProps)
+  }
+
   const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = (request) => {
     const error = request.error as PossibleErrors
 
     console.log({ error })
 
-    const response = (error as ApiError).API_ERROR !== undefined
-      ? new Response({
+    let response
+
+    if ((error as ApiError).API_ERROR !== undefined) {
+      response = new Response({
         statusCode: (error as ApiError).httpStatusCodeError,
         body: new Body(error.message)
       })
-      : new Response({
+    } else if ((error as AwsError).code !== undefined) {
+      response = treatAwsError(error as AwsError)
+    } else {
+      response = new Response({
         statusCode: 500,
         body: new Body('Treated internal server Error')
       })
+    }
 
     return response.getValue()
   }
