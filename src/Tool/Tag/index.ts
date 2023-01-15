8e10b015c199cb63485908cb969676fcf414b1f5
IagorSs <iagor.penaranda@uol.com.br>
2023-01-15 11:32:35 -0300

    feat: change Errors to ApiErrors

--- src/Tool/Tag/index.ts
+++ src/Tool/Tag/index.ts
@@ -1,10 +1,16 @@
+import { ApiError } from '@core/Error'
 import TagOptions from './TagOptions'
 
 export default class Tag {
   private readonly value: TagOptions
 
   constructor (value: string) {
-    if (!Object.keys(TagOptions).includes(value)) throw Error('Cannot set this value to Tag')
+    if (!Object.keys(TagOptions).includes(value)) {
+      throw new ApiError({
+        message: 'Cannot set this value to Tag',
+        httpStatusCodeError: 400
+      })
+    }
 
     this.value = (TagOptions as any)[value]
   }
